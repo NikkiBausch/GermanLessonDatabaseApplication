f9ce400e80955e3d0290c90d13825f46d32da47b
NikkiBeeLili <arcanahereticae13@gmail.com>
2022-02-20 19:52:45 -0600

    Initial Commit

---
+++ src/main/java/org/launchcode/GermanLessonDatabase/GermanLessonDatabaseApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.GermanLessonDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermanLessonDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermanLessonDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/GermanLessonDatabase/GermanLessonDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.GermanLessonDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermanLessonDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
